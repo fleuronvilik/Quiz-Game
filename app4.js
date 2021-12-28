985beffb37b8666b538b3fc05139a102905c7b6b
AdodoolaO <adodoola.omiyale@outlook.com>
2021-12-28 12:42:08 +0000

    Added JS

--- app4.js
+++ app4.js
@@ -8,17 +8,28 @@ axios.get(apiUrl).then(showQuestion);
 function showQuestion(response) {
   console.log(response.data.results);
   let results = response.data.results;
+
   results.forEach(function (result) {
     console.log(result.question);
     questions = result.question;
     let questionDisplay = document.querySelector("#question");
     questionDisplay.innerHTML = result.question;
-    let answerDisplay = document.querySelector("#answer");
-    answerDisplay.innerHTML = result.correct_answer;
+    console.log(result.incorrect_answers);
+    let answerOne = document.querySelector("#firstAnswer");
+    answerOne.innerHTML = result.incorrect_answers[0];
+    let answerTwo = document.querySelector("#secondAnswer");
+    answerTwo.innerHTML = result.incorrect_answers[1];
+    let answerCorrect = document.querySelector("#correctAnswer");
+    answerCorrect.innerHTML = result.correct_answer;
+    let answerThree = document.querySelector("#thirdAnswer");
+    answerThree.innerHTML = result.incorrect_answers[2];
   });
 }
-function revealAnswer() {
-  alert("hi");
-}
-let answer = document.getElementById("answerButton");
-answer.addEventListener("click", revealAnswer);
+
+//function revealAnswer() {
+//let apiUrl = " https://opentdb.com/api.php?amount=50";
+//axios.get(apiUrl).then(showQuestion);
+//}
+
+//let answer = document.getElementById("answerButton");
+//answer.addEventListener("click", revealAnswer);
