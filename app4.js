c132c1ec97b07c6079422e6fe88468a520f9e075
AdodoolaO <adodoola.omiyale@outlook.com>
2022-01-05 22:06:55 +0000

    Added functions

--- app4.js
+++ app4.js
@@ -3,7 +3,9 @@ let apiUrl = " https://opentdb.com/api.php?amount=50";
 let questions = [];
 let questionIndex = 0;
 
-axios.get(apiUrl).then(function (response) {
+axios.get(apiUrl).then(showQuestion);
+
+function showQuestion(response) {
   let questions = response.data.results;
   console.log(questions);
   console.log(questions[questionIndex]);
@@ -12,4 +14,11 @@ axios.get(apiUrl).then(function (response) {
   questionPART.innerHTML = questions[questionIndex].question;
   let categoryPart = document.querySelector("#category");
   categoryPart.innerHTML = questions[questionIndex].category;
-});
+}
+
+function nextQuestion() {
+  axios.get(apiUrl).then(showQuestion);
+}
+
+let next = document.getElementById("next");
+next.addEventListener("click", nextQuestion);
