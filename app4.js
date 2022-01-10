62ffdd35f38c7e13c30d7460debe23d80852a449
AdodoolaO <adodoola.omiyale@outlook.com>
2022-01-10 20:49:52 +0000

    added answer options to js

--- app4.js
+++ app4.js
@@ -1,32 +1,48 @@
-let apiUrl = " https://opentdb.com/api.php?amount=50";
+let apiUrl = "https://opentdb.com/api.php?amount=50";
 
 let questions = [];
 let questionIndex = 0;
 
 axios.get(apiUrl).then(showQuestion);
 
+//created a function to (1) recieve API information (2) Include info in HTml (3) Make answer options random
+
 function showQuestion(response) {
   let questions = response.data.results;
   console.log(questions);
   console.log(questions[questionIndex]);
+
   console.log(questions[questionIndex].question);
-  let questionPART = document.querySelector("#question");
-  questionPART.innerHTML = questions[questionIndex].question;
+  let questionPart = document.querySelector("#question");
+  questionPart.innerHTML = questions[questionIndex].question;
   let categoryPart = document.querySelector("#category");
   categoryPart.innerHTML = questions[questionIndex].category;
-  console.log(questions[questionIndex].correct_answer);
+
   let correctAnswer = questions[questionIndex].correct_answer;
 
-  function revealAnswer() {
-    alert(`The correct answer is ${correctAnswer}!`);
-  }
-  let answer = document.getElementById("answerButton");
-  answer.addEventListener("click", revealAnswer);
+  console.log(correctAnswer);
+  console.log(questions[questionIndex].incorrect_answers[0]);
+  let incorrectAnswer = questions[questionIndex].incorrect_answers;
+
+  let answerOne = document.querySelector("#firstAnswer");
+  answerOne.innerHTML = correctAnswer;
+  let answerTwo = document.querySelector("#secondAnswer");
+  answerTwo.innerHTML = questions[questionIndex].incorrect_answers[0];
+  let answerThree = document.querySelector("#thirdAnswer");
+  answerThree.innerHTML = questions[questionIndex].incorrect_answers[1];
+  let answerFour = document.querySelector("#fourthAnswer");
+  answerFour.innerHTML = questions[questionIndex].incorrect_answers[2];
 }
+function checkAnswer() {
+  alert("answerOne");
+}
+
+let showAnswer = document.querySelector("#answerButton");
+showAnswer.addEventListener("click", checkAnswer);
 
 function nextQuestion() {
   axios.get(apiUrl).then(showQuestion);
 }
 
-let next = document.getElementById("next");
-next.addEventListener("click", nextQuestion);
+let nextButton = document.querySelector("#next");
+nextButton.addEventListener("click", nextQuestion);
