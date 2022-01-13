6409402b357770d974d8552a90392908eb04f43f
Fleuron Gnadekpa <vilfleur@gmail.com>
2022-01-13 10:44:26 +0100

    Add a function to shuffle answers and
    rename index4.html for gh-pages

--- app4.js
+++ app4.js
@@ -13,24 +13,31 @@ function showQuestion(response) {
   console.log(questions);
   console.log(questions[questionIndex]);
 
-  console.log(questions[questionIndex].question);
+  // console.log(questions[questionIndex].question);
   let questionPart = document.querySelector("#question");
   questionPart.innerHTML = questions[questionIndex].question;
   let categoryPart = document.querySelector("#category");
   categoryPart.innerHTML = questions[questionIndex].category;
 
   let correctAnswer = questions[questionIndex].correct_answer;
+  let answers = questions[questionIndex].incorrect_answers;
+  // console.log(answers)
+  answers.push(correctAnswer)
+  // console.log(answers)
 
-  console.log(correctAnswer);
+  // console.log(correctAnswer);
+
+  let shuffledAnswers = shuffleAnswers(answers)
+  console.log(shuffledAnswers)
 
   let answerOne = document.querySelector("#firstAnswer");
-  answerOne.innerHTML = correctAnswer;
+  answerOne.innerHTML = shuffledAnswers[0];
   let answerTwo = document.querySelector("#secondAnswer");
-  answerTwo.innerHTML = questions[questionIndex].incorrect_answers[0];
+  answerTwo.innerHTML = shuffledAnswers[1];
   let answerThree = document.querySelector("#thirdAnswer");
-  answerThree.innerHTML = questions[questionIndex].incorrect_answers[1];
+  answerThree.innerHTML = shuffledAnswers[2];
   let answerFour = document.querySelector("#fourthAnswer");
-  answerFour.innerHTML = questions[questionIndex].incorrect_answers[2];
+  answerFour.innerHTML = shuffledAnswers[3];
   function checkAnswer() {
     alert(correctAnswer);
   }
@@ -45,3 +52,18 @@ function nextQuestion() {
 
 let nextButton = document.querySelector("#next");
 nextButton.addEventListener("click", nextQuestion);
+
+function shuffleAnswers(answersArray) {
+  let answersInRandomOrder = []
+  for (let i = 0, l = answersArray.length; i < l; i++) {
+    // console.log(i)
+    let randomIndex = Math.floor(Math.random() * answersArray.length)
+    // console.log(randomIndex)
+    let item = answersArray.splice(randomIndex, 1)
+    // console.log(item)
+    answersInRandomOrder = answersInRandomOrder.concat(item)
+    // console.log(answersInRandomOrder)
+    // delete answersArray[randomIndex]
+  }
+  return answersInRandomOrder
+}
